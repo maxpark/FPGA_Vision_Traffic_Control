277ea9f1b892d5ceaa49cdf4fd9902bd56473267
Aba <abarajithan07@gmail.com>
2019-06-12 23:32:12 +0000

    tests on speeding up conv. multiprocessing the inner loop gives same result. found a stackoverflow answer with 'stride_tricks' in np. That gives 7x to 9x improvement. But answer not accurate.

--- nn/framework/multi.py
+++ nn/framework/multi.py
@@ -1,5 +1,5 @@
 import os
-from multiprocessing import Process
+from multiprocessing import Process, Lock
 
 import numpy as np
 from scipy.signal import convolve2d
@@ -8,23 +8,70 @@ im_full = np.random.randn(228, 228, 32)
 kernel_full = np.random.randn(3, 3, 32, 64)
 
 
-def conv_out(im_full, kernel_full, out, out_ch_i):
+def conv_out(im_full, kernel_full, out, out_ch_i, lock):
     out_ch = np.empty((228, 228))
 
     for in_ch_i in range(32):
         out_ch += convolve2d(im_full[:, :, in_ch_i],
                              kernel_full[:, :, in_ch_i, out_ch_i], 'same')
-
     out[:, :, out_ch_i] = out_ch
+    # lock.acquire()
+    # try:
+    #     out[:, :, out_ch_i] = out_ch
+    # finally:
+    #     lock.release()
 
 
 def loop_conv(im_full, kernel_full):
     out = np.empty((228, 228, 64))
 
     for out_ch_i in range(64):
-        conv_out(im_full, kernel_full, out, out_ch_i)
+        conv_out(im_full, kernel_full, out, out_ch_i, None)
 
     return out
+
+
+# print(loop_conv(im_full, kernel_full))
+
+a = [1, 2, 3]
+
+
+def abc(b, i, lock):
+    lock.acquire()
+    b[i] = 15
+    lock.release()
+
+
+if __name__ == "__main__":
+    procs = []
+    lock = Lock()
+
+    for i in range(3):
+        proc = Process(target=abc, args=(a, i, lock,))
+
+    for proc in procs:
+        proc.join()
+
+    print(a)
+
+
+if __name__ != "__main__":
+
+    procs = []
+    lock = Lock()
+
+    out = np.empty((228, 228, 64))
+
+    for out_ch_i in range(64):
+        proc = Process(target=conv_out, args=(im_full, kernel_full,
+                                              out, out_ch_i, lock,))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+
+    print(np.all(out == np.empty((228, 228, 64))))
 
 
 # if __name__ == '__main__':
